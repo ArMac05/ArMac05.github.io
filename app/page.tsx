7e4ba04ac4e7b7d3aa15e50967a3ebeb341a054c
Arlim Macaldo <arlimmacaldo@icloud.com>
2026-07-28 08:23:04 -0700

    feat: Added a hero section component. main page now contains the hero section. Anything inside the hero will be in the component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,22 @@
 import { Tag } from "@/components";
+import HeroSection from "@/components/HeroSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
 import AboutSection from "@/components/AboutSection";
+import { ExperienceSection } from "@/components/ExperienceSection";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans bg-white">
+    <div className="bg-white">
       {/* Hero Card */}
-      <div className="w-screen h-screen bg-hero-background">
-        <div className="flex flex-col items-start max-w-1/2 m-24 mt-36 gap-6">
-          <Tag
-            text="Open to Internships"
-            className="flex flex-column items-center justify-center bg-Cream/12 w-48 h-8 rounded-2xl border border-chip-border font-bold text-Cream text-center"
-          ></Tag>
-          <div className="">
-            <p className="font-bold text-6xl text-Cream">
-              Hi, I'm Arlim Macaldo
-            </p>
-          </div>
-          <div>
-            <p className="font-medium text-3xl text-Cream-Dimmed">
-              CS Student at UNLV, focused on data engineering - building
-              reliable pipelines and turning messy data into something usable.
-            </p>
-          </div>
-          <button className="bg-Coral font-bold text-base rounded-xl w-34 h-12">
-            View Resume
-          </button>
-        </div>
-      </div>
+      <HeroSection />
       {/* About Me */}
       <AboutSection />
 
       {/* Projects */}
       <ProjectsSection />
+
+      {/* Experiences */}
+      <ExperienceSection />
     </div>
   );
 }
