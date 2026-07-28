a833c93e4ec58ba0f89c301677c6de616802b1a2
Arlim Macaldo <arlimmacaldo@icloud.com>
2026-07-27 19:06:05 -0700

    feat: downloaded tailwind-merge dependency. modified skills type and data.

--- data/skills.ts
+++ data/skills.ts
@@ -1,6 +1,24 @@
-export const skills = {
-  Languages: ["Python", "SQL", "React", "C++"],
-  "Data & Orchestration": ["DBT", "Apache Airlfow", "Spark"],
-  "Storage & Cloud": ["BigQuery", "PostgreSQL", "DuckDB"],
-  "Dev Tools": ["VScode", "Docker", "Git", "Databricks"],
-};
+import { Skill } from "@/types/skills";
+
+export const skills: Skill[] = [
+  {
+    id: "languages",
+    category: "Langauges",
+    name: ["Python", "SQL", "React", "C++"],
+  },
+  {
+    id: "data & orchestration",
+    category: "Data & Orchestration",
+    name: ["DBT", "Apache Airflow", "Spark"],
+  },
+  {
+    id: "storage & cloud",
+    category: "Storage & Cloud",
+    name: ["BigQuery", "PostgreSQL", "DuckDB"],
+  },
+  {
+    id: "dev tools",
+    category: "Dev Tools",
+    name: ["VScode", "Docker", "Git", "Databricks"],
+  },
+];
---
+++ types/skills.ts
@@ -0,0 +1,8 @@
+export interface Skill {
+  /** Id for category */
+  id: string;
+  /** Name of the category */
+  category: string;
+  /** name of each skill. Max of 4 */
+  name: string[];
+}
