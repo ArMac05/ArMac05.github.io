97308af6751771ecb39722bf0ea1d4fdf02173c6
Arlim Macaldo <arlimmacaldo@icloud.com>
2026-07-29 20:16:11 -0700

    fix(Hero): fixed hero section where the margin is not aligning with the other sections in the website.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,25 +3,29 @@ export default function HeroSection() {
   return (
     <section
       id="hero"
-      className="w-full h-screen bg-hero-background px-12 py-16 flex items-center"
+      className="w-full min-h-screen bg-hero-background flex items-center"
     >
-      <div className="flex flex-col items-start gap-6 max-w-3xl">
-        <Tag
-          text="Open to Internships"
-          className="flex flex-col items-center justify-center bg-cream/12 w-48 h-8 rounded-2xl border border-chip-border font-bold text-cream text-center"
-        ></Tag>
-        <div className="">
-          <p className="font-bold text-6xl text-cream">Hi, I'm Arlim Macaldo</p>
+      <div className="w-full max-w-6xl mx-auto px-6 md:px-12">
+        <div className="flex flex-col items-start gap-6 max-w-3xl">
+          <Tag
+            text="Open to Internships"
+            className="flex flex-col items-center justify-center bg-cream/12 w-48 h-8 rounded-2xl border border-chip-border font-bold text-cream text-center"
+          ></Tag>
+          <div className="">
+            <p className="font-bold text-6xl text-cream">
+              Hi, I'm Arlim Macaldo
+            </p>
+          </div>
+          <div>
+            <p className="font-medium text-3xl text-cream-dimmed">
+              CS Student at UNLV, focused on data engineering - building
+              reliable pipelines and turning messy data into something usable.
+            </p>
+          </div>
+          <button className="bg-coral font-bold text-base rounded-xl w-34 h-12">
+            View Resume
+          </button>
         </div>
-        <div>
-          <p className="font-medium text-3xl text-cream-dimmed">
-            CS Student at UNLV, focused on data engineering - building reliable
-            pipelines and turning messy data into something usable.
-          </p>
-        </div>
-        <button className="bg-coral font-bold text-base rounded-xl w-34 h-12">
-          View Resume
-        </button>
       </div>
     </section>
   );
